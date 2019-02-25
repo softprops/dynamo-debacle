2312417d0d6a7418011d6fbb45be20e8032dce69
softprops <d.tangren@gmail.com>
2019-02-25 00:22:13 -0500

    fail some requests

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,30 @@
+use futures::future::{self, Either};
 use hyper::{
     rt::{self, Future},
     service::service_fn,
-    Client, Server,
+    Body, Client, Response, Server,
 };
 use std::net::SocketAddr;
+
+// https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/Programming.Errors.html
+fn fail() -> Response<Body> {
+    Response::builder()
+        .status(400)
+        .header(
+            "content-type", " application/x-amz-json-1.0"
+        )
+        .body(
+            r#"{"__type":"com.amazonaws.dynamodb.v20120810#ProvisionedThroughputExceededException",
+    "message":"You exceeded your maximum allowed provisioned throughput for a table or for one or more global secondary indexes. To view performance metrics for provisioned throughput vs. consumed throughput, open the Amazon CloudWatch console."}"#.into()
+        )
+        .unwrap()
+}
 
 fn main() {
     let client = Client::new();
     let in_addr = ([127, 0, 0, 1], 8001).into();
     let out_addr: SocketAddr = ([127, 0, 0, 1], 8000).into();
+
     let server = Server::bind(&in_addr)
         .serve(move || {
             let request_client = client.clone();
@@ -16,7 +32,16 @@ fn main() {
             // `service_fn_ok` is a helper to convert a function that
             // returns a Response into a `Service`.
             service_fn(move |mut req| {
-                println!("{:?}", req.headers());
+                if let Some(target) = req.headers().get("x-amz-target") {
+                    if let Some(operation) =
+                        target.to_str().ok().and_then(|s| s.splitn(2, '.').last())
+                    {
+                        if rand::random::<f64>() > 0.5 {
+                            println!("failing {} operation", operation);
+                            return Either::A(future::ok(fail()));
+                        }
+                    }
+                }
                 let uri_string = match req.uri().path_and_query().map(|x| x.as_str()) {
                     Some(path) if !path.is_empty() => format!("http://{}{}", out_addr, path),
                     _ => format!("http://{}", out_addr),
@@ -24,7 +49,7 @@ fn main() {
                 println!("proxying to {}", uri_string);
                 let uri = uri_string.parse().unwrap();
                 *req.uri_mut() = uri;
-                request_client.request(req)
+                Either::B(request_client.request(req))
             })
         })
         .map_err(|e| eprintln!("server error: {}", e));
